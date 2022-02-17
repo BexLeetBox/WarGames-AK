8f72d56881ddcb4714eb08f62f22e368f88b3f06
DeDcon0 <93771670+BexLeetBox@users.noreply.github.com>
2022-02-17 17:42:08 +0100

    added commander Unit class

---
+++ src/main/java/edu/ntnu/idatt2001/Entities/CommanderUnit.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2001.Entities;
+
+public class CommanderUnit extends CavalryUnit{
+
+    public CommanderUnit(String name, int health, int attack, int armor) {
+        super(name, health, 25, 15);
+    }
+
+    public CommanderUnit(String name, int health) {
+        super(name, health);
+    }
+
+    
+
+}
